251b6691b4e5dc6cb0606ae66dcff6ee29c8adc1
Aymix <aymenhmida1@gmail.com>
2026-09-08 21:33:50 +0100

    Fix: path warning

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  resolve: { alias: { "@": path.resolve(import.meta.dirname, "./src"), } },
 });
