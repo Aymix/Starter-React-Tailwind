732e2fd85af70d6f31f49a7bd9bb70ae0831dd1d
Aymix <aymenhmida1@gmail.com>
2026-09-08 21:34:07 +0100

    Feat: Add core features

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
+import "@/index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "@/App.tsx";
+import { BrowserRouter } from "react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     <App />
   </StrictMode>,
 );
